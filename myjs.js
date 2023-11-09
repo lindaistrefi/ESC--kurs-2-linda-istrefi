064e9bf2ff15999a2121e085300caf47ea607ee8
Linda Istrefi <linda.istrefi@lerniastudent.se>
2023-11-09 22:16:56 +0100

    created js-file

---
+++ myjs.js
@@ -0,0 +1,38 @@
+// my variables
+
+const openMenu = document.querySelector("..headerIntro__nav--openMenu")
+const closeBtn = document.querySelector(".headerIntro__nav--closeMenu");
+const popUpMenu = document.querySelector(".headerIntro__nav")
+const boxBack = document.querySelector(".headerIntro__nav--back");
+const theBox = document.querySelector("#mobile-menu")
+
+
+// adding eventListners to open and close mobile menu
+
+      //open menu
+
+openMenu.addEventListener(onclick, open);
+
+      //close menu
+
+closeBtn.addEventListener(onclick, close);      
+
+
+//adding my functions 
+
+function open() {
+
+theBox.style.display = property;
+
+
+}
+
+  
+//functions
+
+
+  
+
+
+
+
