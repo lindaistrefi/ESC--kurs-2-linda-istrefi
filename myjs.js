4994cb2151f9eea50809beb80ccbcc7da8ee0833
Linda Istrefi <linda.istrefi@lerniastudent.se>
2023-11-10 00:44:14 +0100

    stuctural changes

--- myjs.js
+++ myjs.js
@@ -1,37 +1,40 @@
 // my variables
 
-const openMenu = document.querySelector("..headerIntro__nav--openMenu")
+const openMenu = document.querySelector(".headerIntro__nav--openMenu")
 const closeBtn = document.querySelector(".headerIntro__nav--closeMenu");
-const popUpMenu = document.querySelector(".headerIntro__nav")
+const popUpMenu = document.querySelector(".headerIntro__navTwo")
 const boxBack = document.querySelector(".headerIntro__nav--back");
 const theBox = document.querySelector("#mobile-menu")
-
+const myHTML = document.querySelector("html");
 
 // adding eventListners to open and close mobile menu
 
       //open menu
 
-openMenu.addEventListener(onclick, open);
+openMenu.addEventListener(click, open);
 
       //close menu
 
-closeBtn.addEventListener(onclick, close);      
+closeBtn.addEventListener(click, close);      
 
 
 //adding my functions 
 
-function open() {
+function open(property) {
 
-theBox.style.display = property;
-
-
+popUpMenu.style.visibility = "hidden";
 }
 
-  
-//functions
+
+function close (property) {
+
+  popUpMenu.style.visibility = "visible";
+}
 
 
-  
+
+
+
 
 
 
